3ae80ee96c241ae2d1bfd4cdf5645279019b2b9f
francisco-1998 <francmarinc@gmail.com>
2020-12-03 11:55:37 -0500

    Configuracion de un modelo para gestionar la data que devuelve el qrScan

---
+++ src/app/models/registro.model.ts
@@ -0,0 +1,37 @@
+export class Registro {
+    public formato: string;
+    public texto: string;
+    public type: string;
+    public icon: string;
+    public creado: Date;
+
+    constructor(format: string, text: string) {
+        this.formato = format;
+        this.texto = text;
+        this.creado = new Date();
+        this.determinarTipo();
+    }
+
+
+    private determinarTipo(){
+        const inicioTexto = this.texto.substr(0,8);
+        console.log('Tipo:', inicioTexto);
+
+        switch (inicioTexto) {
+            case 'http':
+                this.type = 'Sitio web'
+                this.icon = 'globe-outline'
+                break;
+
+            case 'geo:':
+                this.type = 'Mapa'
+                this.icon = 'map-outline'
+                break;
+        
+            default:
+                this.type = 'No configurado'
+                this.icon = 'pencil-outline'
+                break;
+        }
+    }
+}
